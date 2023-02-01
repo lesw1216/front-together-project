8df94a942ddb6dce16c839bf90ae0384e3ea4945
lesw <lesw1216@gmail.com>
2023-02-02 03:25:25 +0900

    pinia down, setting completion

--- src/views/unAuthenticated/Login.vue
+++ src/views/unAuthenticated/Login.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { useRouter } from "vue-router";
+import { ref } from "vue";
+
 const router = useRouter();
 const login = () => {
   router.push({
@@ -10,14 +12,22 @@ const login = () => {
 const join = () => {
   router.push({ path: "/join" });
 };
+
+const user = ref({
+  name: "",
+  passwrod: "",
+});
 </script>
 <template>
   <div class="board">
     <h1>Together</h1>
-    <input type="text" placeholder="ID" />
-    <input type="password" placeholder="PASSWORD" />
+    <input v-model="user.name" type="text" placeholder="ID" />
+    <input v-model="user.password" type="password" placeholder="PASSWORD" />
     <button @click="login()">로그인</button>
     <button @click="join()">회원 가입</button>
+    {{ user.name }}
+    {{ user.password }}
+    <div></div>
   </div>
 </template>
 <style scoped>
