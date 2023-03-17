bb1ac4c6e01f3d1850f8f99c9fcb81cf74d30ba1
lesw <lesw1216@gmail.com>
2023-03-17 22:29:56 +0900

    글작성 돌아가기 버튼 클릭시 모달창 동작 완료

--- src/components/Notices/Write.vue
+++ src/components/Notices/Write.vue
@@ -1,7 +1,27 @@
 <script setup>
-console.log("write");
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const isModal = ref(false);
+
+const OnClickModal = () => {
+  isModal.value = !isModal.value;
+  console.log(isModal.value);
+};
+
+const OnClickModalOk = () => {
+  router.push({
+    path: "/notice",
+  });
+};
 </script>
 <template>
+  <div class="z-40" v-if="isModal">
+    <Modal @modal-cancel="OnClickModal" @modal-ok="OnClickModalOk"></Modal>
+  </div>
   <div class="h-full w-full flex flex-col">
     <header>
       <input
@@ -15,15 +35,15 @@ console.log("write");
         class="p-2 w-full h-full resize-none border-black border-2 focus:border-2 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-400 text-xl"
       ></textarea>
     </section>
-    <footer class="flex">
+    <footer class="flex justify-end">
       <button
-        class="border-2 mb-2 mr-2 px-4 py-2 border-indigo-400 hover:bg-indigo-400 hover:text-white basis-1/2"
+        class="border-2 mb-2 mr-2 px-4 py-2 border-violet-600 bg-violet-600 text-white font-bold rounded-sm text-xl"
       >
         작성하기
       </button>
       <button
-        class="border-2 px-4 py-1 mb-2 border-rose-400 hover:bg-rose-400 hover:text-white basis-1/2"
-        @click="$router.push('/notice')"
+        class="border-2 px-4 py-1 mb-2 border-gray-600 font-bold rounded-sm text-xl"
+        @click="OnClickModal"
       >
         돌아가기
       </button>
