7bc51181472180194eb5f107b463578e7f1a9629
lesw <lesw1216@gmail.com>
2023-03-22 14:33:38 +0900

    게시글 조회, 수정 화면 디자인 완성

--- src/components/Notices/Read.vue
+++ src/components/Notices/Read.vue
@@ -1,15 +1,23 @@
 <script setup>
 import { useNavBarStore } from "@/stores/navBar";
 import { useRouter } from "vue-router";
+import { ref } from "vue";
 
 const router = useRouter();
 const useNavBar = useNavBarStore();
+
+const isUpdate = ref(false);
 
 const OnClickToListButton = () => {
   useNavBar.setIsMainDisplay(true);
   router.push({
     path: "/notice",
   });
+};
+
+const OnClickUpdateButton = () => {
+  isUpdate.value = true;
+  console.log(isUpdate.value);
 };
 </script>
 <template>
@@ -24,6 +32,7 @@ const OnClickToListButton = () => {
         <div class="mt-10 flex justify-between">
           <div>작성자</div>
           <div class="flex items-end">
+            <button class="mr-2" @click="OnClickUpdateButton">수정</button>
             <div class="mr-2">작성일자</div>
             <div>조회수</div>
           </div>
@@ -32,13 +41,22 @@ const OnClickToListButton = () => {
       <section class="basis-full mb-2">
         <textarea
           class="w-full h-full resize-none focus:outline-none read-only:border-2 read-only:border-violet-200 shadow-sm text-2xl"
-          readonly
-        ></textarea>
+          v-bind:readonly="!isUpdate"
+        >
+sdfsdfsdf</textarea
+        >
       </section>
-      <footer class="">
+      <footer class="basis-auto flex flex-col">
         <button
           @click="OnClickToListButton"
-          class="border border-slate-500 hover:border-emerald-500 hover:text-emerald-600 w-full h-full p-2 text-center"
+          class="border border-blue-600 text-blue-700 w-full p-2 text-center mb-2"
+          v-if="isUpdate"
+        >
+          완료
+        </button>
+        <button
+          @click="OnClickToListButton"
+          class="border border-slate-500 hover:border-emerald-500 hover:text-emerald-600 w-full p-2 text-center"
         >
           글 목록
         </button>
